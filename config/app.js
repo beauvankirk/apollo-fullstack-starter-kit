afe0a9679818e0b4ef8db173cbe55c645a28c1e1
Beau Van Kirk <beauvankirk@gmail.com>
2021-02-05 13:54:55 -0600

    Changes on previous work phase

--- config/app.js
+++ config/app.js
@@ -3,7 +3,7 @@ export default {
   logo: 'logo.svg', // Loaded via webpack and stored in favicon/common/assets
   logging: {
     level: (process.env.npm_config_argv || '').search(/(watch|start)/) >= 0 ? 'debug' : 'info',
-    debugSQL: false,
+    debugSQL: true,
     apolloLogging: (process.env.npm_config_argv || '').search(/(watch|start)/) >= 0
   },
   // Check here for Windows and Mac OS X: https://code.visualstudio.com/docs/editor/command-line#_opening-vs-code-with-urls
